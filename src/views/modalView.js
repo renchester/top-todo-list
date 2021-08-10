257f17074932d5b17bdc596784fd25cc2436cb68
Renchester Ramos <renchesterjramos@gmail.com>
2021-08-10 16:07:54 +0800

    Add modalView display switching function

--- src/views/modalView.js
+++ src/views/modalView.js
@@ -5,6 +5,7 @@ export default class ModalView extends View {
 
   _parentElement = document.querySelector('.add-task--content');
   _window = document.querySelector('.modal--add-task');
+  _sidebarLabels = document.querySelectorAll('.sidebar--label');
 
   _btnOpen = document.querySelector('.btn.add-task');
   _btnClose = document.querySelector('.material-icons.icon--close-modal');
@@ -14,18 +15,68 @@ export default class ModalView extends View {
     super();
     this._addHandlerShowModal();
     this._addHandlerCloseModal();
+    this._addHandlerModalSidebar();
   }
 
   _addHandlerShowModal() {
+    // Shows/opens the modal
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
   }
 
   _addHandlerCloseModal() {
+    // Closes the modal
     this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerModalSidebar() {
+    // Changes the form display
+    this._sidebarLabels.forEach((el) =>
+      el.addEventListener('click', this.toggleFormDisplay.bind(this))
+    );
   }
 
   toggleWindow() {
     // this._overlay.classList.toggle('hidden');
     this._window.classList.toggle('hidden');
+  }
+
+  toggleFormDisplay(e) {
+    // Hides all forms
+    this._parentElement
+      .querySelectorAll('.content-form')
+      .forEach((el) => el.classList.add('hidden'));
+
+    const formType = Array.from(e.target.classList)[1].split('new-')[1];
+
+    this._clearForm();
+
+    this._parentElement
+      .querySelector(`.content-form--new-${formType}`)
+      .classList.remove('hidden');
+  }
+
+  _clearForm() {
+    // Clear task form
+    this._parentElement
+      .querySelectorAll('.new-task--form-element')
+      .forEach((el) => (el.value = ''));
+
+    // Clear project form
+    this._parentElement
+      .querySelectorAll('.new-project--form-element')
+      .forEach((el) => (el.value = ''));
+
+    // Clear note form
+    this._parentElement
+      .querySelectorAll('.new-note--form-element')
+      .forEach((el) => (el.value = ''));
+
+    // Clear priority choice
+    this._parentElement
+      .querySelectorAll('.new-task--task-priority-label')
+      .forEach((el) => el.classList.remove('priority-label--active'));
+
+    // Clear task date
+    this._parentElement.querySelector('.new-task--task-date').value = '';
   }
 }
