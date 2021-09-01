547d3e9913b1e28fd2284f24a7c0c6edb6c732c3
Renchester Ramos <renchesterjramos@gmail.com>
2021-09-01 17:39:32 +0800

    Add hamburger menu for portrait orientation

--- src/views/sidebarView.js
+++ src/views/sidebarView.js
@@ -3,9 +3,19 @@ import View from './view';
 class SidebarView extends View {
   _window = document.querySelector('.nav-bar');
   _parentElement = document.querySelector('.nav-subdiv.project--list');
+  _hamburger = document.querySelector('.hamburger');
+  _navHeaders = document.querySelector('.nav-headers');
+  _mediaQueryPortrait = window.matchMedia('(orientation: portrait)');
 
   constructor() {
     super();
+    this._addHandlerShowNavHeaders();
+    this._addHandlerShowProjectDropDown();
+    this._mediaQueryPortrait.addEventListener(
+      'change',
+      this._handleOrientationChange.bind(this)
+    );
+    this._initNavHeaders();
   }
 
   _generateMarkup() {
@@ -18,6 +28,52 @@ class SidebarView extends View {
     const markup = `
     <li class="nav-subdiv--content project--title">${project.title}</li>`;
     return markup;
+  }
+
+  _handleOrientationChange(e) {
+    if (e.matches) {
+      this._matchPortrait();
+    } else if (!e.matches) {
+      this._matchLandscape();
+    }
+  }
+
+  _initNavHeaders() {
+    if (this._mediaQueryPortrait.matches) {
+      this._matchPortrait();
+    } else if (!this._mediaQueryPortrait.matches) {
+      this._matchLandscape();
+    }
+  }
+
+  _matchPortrait() {
+    this._navHeaders.classList.add('hidden');
+    this._hamburger.classList.remove('hidden');
+  }
+
+  _matchLandscape() {
+    this._navHeaders.classList.remove('hidden');
+    this._hamburger.classList.add('hidden');
+  }
+
+  _showNavHeadersDisplay() {
+    this._navHeaders.classList.toggle('hidden');
+  }
+
+  _addHandlerShowNavHeaders() {
+    this._hamburger.addEventListener(
+      'click',
+      this._showNavHeadersDisplay.bind(this)
+    );
+  }
+  _addHandlerShowProjectDropDown() {
+    document
+      .querySelector('.nav-header.header--projects')
+      .addEventListener('click', function () {
+        document
+          .querySelector('.nav-subdiv.project--list')
+          .classList.toggle('project--list-visible');
+      });
   }
 
   addHandlerShowProject(handler) {
