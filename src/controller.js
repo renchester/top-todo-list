245c92799f135c6403cdd646039814f3e1d8bcbf
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-02 02:57:17 +0800

    Create controllers for adding content to state

--- src/controller.js
+++ src/controller.js
@@ -3,5 +3,56 @@ import styles from '/src/style.css';
 import * as model from './model';
 
 import View from './views/View';
+import taskView from './views/taskView';
+import taskDetailsView from './views/taskDetailsView';
+import addTaskView from './views/addTaskView';
+import addProjectView from './views/addProjectView';
+import addNoteView from './views/addNoteView';
 
-const controlTasks = function () {};
+// Task Controllers
+
+const controlAddTask = (data) => {
+  model.addTask(data);
+
+  // Close form & show tasks
+};
+
+const controlShowAllTasks = () => {
+  taskView.render(model.state.tasks);
+};
+
+const controlTaskDetails = (id) => {
+  const targetTask = model.state.tasks.find((task) => task.id === id);
+
+  taskDetailsView.render(targetTask);
+};
+
+const controlEditTask = () => {};
+
+// Project Controllers
+
+const controlAddProject = (data) => {
+  model.addProject(data);
+};
+
+const controlShowProjectsOnForm = () => {
+  addTaskView.renderProjects(model.state.projects);
+};
+
+// Note Controllers
+
+const controlAddNote = (data) => {
+  model.addNote(data);
+};
+
+const init = () => {
+  controlShowAllTasks();
+
+  taskDetailsView.addHandlerTaskDetails(controlTaskDetails);
+  addTaskView.addHandlerAddTask(controlAddTask);
+  addTaskView.addHandlerListProjects(controlShowProjectsOnForm);
+  addNoteView.addHandlerAddNote(controlAddNote);
+  addProjectView.addHandlerAddProject(controlAddProject);
+};
+
+init();
--- src/model.js
+++ src/model.js
@@ -1,7 +1,101 @@
 /* eslint-disable no-underscore-dangle */
 
+export const state = {
+  tasks: [
+    {
+      title: 'Title',
+      details: 'Sample task',
+      priority: 'low',
+      status: 'finished',
+      id: '123456',
+      date: '01-11-1999',
+      project: 'home',
+    },
+    {
+      title: 'Title2',
+      details: 'Sample task2',
+      priority: 'medium',
+      status: 'unfinished',
+      id: '654321',
+      date: '01-11-2011',
+      project: 'home',
+    },
+  ],
+  projects: [
+    { title: 'Home', id: '12345' },
+    { title: 'Europe trip 2021', id: '23456' },
+    { title: 'Asia Trip 2024', id: '34567' },
+    {
+      title:
+        'Asia Trip 2024 Really long really long really long really long really long really long really long really long really long really long really long',
+      id: '45678',
+    },
+  ],
+  notes: [],
+};
+
+const createTask = (data) => ({
+  title: data.title,
+  details: data.details,
+  priority: data.priority,
+  id: `ID${Math.random().toString(16).slice(2)}`,
+  project: data.project,
+  status: data.status,
+  date: data.date,
+  pinned: data.pinned || false,
+});
+
+export const addTask = (data) => {
+  const newTask = createTask(data);
+
+  state.tasks.unshift(newTask);
+};
+
+const createProject = (data) => ({
+  title: data.title,
+  id: `ID${Math.random().toString(16).slice(2)}`,
+});
+
+export const addProject = (data) => {
+  const newProject = createProject(data);
+
+  state.projects.push(newProject);
+};
+
+const createNote = (data) => ({
+  title: data.title,
+  details: data.details,
+  id: `ID${Math.random().toString(16).slice(2)}`,
+});
+
+export const addNote = (data) => {
+  const newNote = createNote(data);
+
+  state.notes.push(newNote);
+};
+
+/*
 export const state = (() => {
-  const _tasks = [];
+  const _tasks = [
+    {
+      title: 'Title',
+      description: 'Sample task',
+      priority: 'low',
+      status: 'finished',
+      id: '123456',
+      date: '01-11-1999',
+      project: 'home',
+    },
+    {
+      title: 'Title2',
+      description: 'Sample task2',
+      priority: 'medium',
+      status: 'unfinished',
+      id: '654321',
+      date: '01-11-2011',
+      project: 'home',
+    },
+  ];
   const _projects = [];
   const _notes = [];
 
@@ -77,6 +171,10 @@ export const Task = function (data) {
     setProject(newProject) {
       this.project = newProject;
     },
+
+    setStatus(newStatus) {
+      this.status = newStatus;
+    },
   };
 
   const task = Object.assign(Object.create(methods), props);
@@ -121,3 +219,4 @@ export const Note = function (data) {
 
   return note;
 };
+*/
