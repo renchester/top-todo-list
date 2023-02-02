2968b7c5da967080576bca1dd50d5943dbdf6b72
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-03 04:20:48 +0800

    Update markup for task view

--- src/views/taskView.js
+++ src/views/taskView.js
@@ -22,14 +22,28 @@ class TaskView extends View {
     );
   };
 
+  addHandlerDeleteTask = (handler) => {
+    const tasks = [...document.querySelectorAll('.task')];
+
+    tasks.forEach((task) =>
+      task.addEventListener('click', (e) => {
+        if (!e.target.parentNode.classList.contains('display-icon-delete'))
+          return;
+
+        const { id } = e.target.closest('.task').dataset;
+
+        handler(id);
+      }),
+    );
+  };
+
   _generateMarkup = () => {
     const markup = this._data
       .map(
         (task) => `
-       <div class="task" data-id="${task.id}">
-            <div class="task-condensed">
-              <div class="task-left">
+         <div class="task priority-${task.priority}" data-id="${task.id}">
 
+              <div class="task-left">
                 <input
                   type="checkbox"
                   name="task-status"
@@ -37,39 +51,27 @@ class TaskView extends View {
                   class="task-status"
                   ${task.status === 'finished' ? 'checked' : ''}
                 />
-
-                <span class="task-title${
+                <span class="task-title ${
                   task.status === 'finished' ? 'task-finished' : ''
                 }">${task.title}</span>
-
               </div>
 
-              <div class="task-details-icon">
+              <div class="task-right">
+                <div class="task-details-icon">
+                  Details
+                </div>
 
-                <span class="material-symbols-outlined">expand_more</span>
-
+                <div class="task-display-icon display-icon-edit task-edit">
+                  <span class="material-symbols-outlined">
+                    edit
+                  </span>
+                </div>
+                <div class="task-display-icon display-icon-delete task-delete">
+                  <span class="material-symbols-outlined">
+                    delete
+                  </span>
+                </div>
               </div>
-            </div>
-            <div class="task-expanded hidden">
-              <div class="task-details">
-                ${task.details}
-              </div>
-              <div class="task-priority">Priority: ${this._capitalizeFirstLetter(
-                task.priority,
-              )}</div>
-              <div class="task-date">Due Date: ${task.date}</div>
-              <div class="task-project">Project: ${this._capitalizeFirstLetter(
-                task.project,
-              )}</div>
-              <div class="task-status">Status: ${this._capitalizeFirstLetter(
-                task.status,
-              )}</div>
-              <div class="btn-container">
-                <button type="button" class="btn-edit-task">Edit</button
-                ><button type="button" class="btn-delete-task">Delete</button>
-              </div>
-            
-            </div>
           </div>
     `,
       )
@@ -80,58 +82,34 @@ class TaskView extends View {
 
   _generateBackup = () => {
     const markup = `
-         <div class="task" data-id="null">
-            <div class="task-condensed">
+          <div class="task priority-medium" data-id="null">
+
               <div class="task-left">
                 <input
                   type="checkbox"
                   name="task-status"
                   id="task-status"
-                  value="Finished"
-                  checked
+                  class="task-status"
                 />
-                <span class="task-title">Sample task</span>
+                <span class="task-title">Sample Task</span>
               </div>
-              <div class="task-details-icon">
-                <span class="material-symbols-outlined">expand_more</span>
+
+              <div class="task-right">
+                <div class="task-details-icon">
+                  Details
+                </div>
+
+                <div class="task-display-icon task-edit">
+                  <span class="material-symbols-outlined">
+                    edit
+                  </span>
+                </div>
+                <div class="task-display-icon task-delete">
+                  <span class="material-symbols-outlined">
+                    delete
+                  </span>
+                </div>
               </div>
-            </div>
-            <div class="task-expanded">
-              <div class="task-details">
-                You are seeing this task because you have not added a task yet. Add one now through the add button!
-              </div>
-              <div class="task-priority">Priority: Low</div>
-              <div class="task-date">Due Date: Jan. 01,  2000</div>
-              <div class="task-project">Project: Home</div>
-              <div class="task-status">Status: Finished</div>
-            </div>
-          </div>
-          
-           <div class="task" data-id="null">
-            <div class="task-condensed">
-              <div class="task-left">
-                <input
-                  type="checkbox"
-                  name="task-status"
-                  id="task-status"
-                  value="Finished"
-                  checked
-                />
-                <span class="task-title">Sample task</span>
-              </div>
-              <div class="task-details-icon">
-                <span class="material-symbols-outlined">expand_more</span>
-              </div>
-            </div>
-            <div class="task-expanded hidden">
-              <div class="task-details">
-                You are seeing this task because you have not added a task yet. Add one now through the add button!
-              </div>
-              <div class="task-priority">Priority: Low</div>
-              <div class="task-date">Due Date: Jan. 01,  2000</div>
-              <div class="task-project">Project: Home</div>
-              <div class="task-status">Status: Finished</div>
-            </div>
           </div>`;
 
     return markup;
@@ -139,54 +117,3 @@ class TaskView extends View {
 }
 
 export default new TaskView();
-
-/*
-const taskView = function () {
-  let taskViewObj = Object.create(View());
-
-  // Properties
-
-  taskViewObj.parentElement = document.querySelector('.content-display');
-
-  // Methods
-
-  taskViewObj.generateMarkup = function () {
-    console.log(this);
-    const markup = this.data
-      .map(
-        (task) => `
-       <div class="task" data-id="${task.id}">
-            <div class="task-condensed">
-              <div class="task-left">
-                <input
-                  type="checkbox"
-                  name="task-status"
-                  id="task-status"
-                  value="Finished"
-                  ${task.status === 'finished' ? 'checked' : ''}
-                />
-                <span class="task-title ${
-                  task.status === 'finished' ? 'task-finished' : ''
-                }">${task.title}</span>
-              </div>
-              <div class="task-details">
-                <span class="material-symbols-outlined"> expand_more </span>
-              </div>
-            </div>
-          </div>
-    `,
-      )
-      .join('');
-
-    return markup;
-  };
-
-  taskViewObj.addHandlerAddTask = function (handler) {
-    document.querySelector('.btn-add').addEventListener('click', handler);
-  };
-
-  return taskViewObj;
-};
-
-export default taskView();
-*/
