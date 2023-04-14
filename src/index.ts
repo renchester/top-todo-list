07949d1232e9da788fde9cb558452e2301b3e051
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-14 20:21:28 +0800

    Refactor app initializer function to separate module

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,10 @@
 import '/src/style.css';
 
-import TaskController from './controllers/taskController';
-import ProjectController from './controllers/projectController';
-import SidebarController from './controllers/sidebarController';
-import NoteController from './controllers/noteController';
+import AuthController from './controllers/authController';
 
-import TaskView from './views/taskView';
-import TaskDetailsView from './views/taskDetailsView';
-import EditTaskView from './views/editTaskView';
-import SidebarView from './views/sidebarView';
-import ProjectView from './views/projectView';
-import NoteView from './views/noteView';
-import AddTaskView from './views/addTaskView';
-import AddProjectView from './views/addProjectView';
-import AddNoteView from './views/addNoteView';
-
-const init = () => {
-  // Task initializers
-  TaskController.ctrlShowAllTasks();
-
-  TaskView.addHandlerToggleStatus(TaskController.ctrlToggleStatus);
-  TaskDetailsView.addHandlerShowTaskDetails(TaskController.ctrlTaskDetails);
-  EditTaskView.addHandlerEditTask(TaskController.ctrlEditTask);
-  TaskView.addHandlerDeleteTask(TaskController.ctrlDeleteTask);
-
-  // Project / Sidebar initializers
-  ProjectController.ctrlRenderProjectsOnSidebar();
-
-  SidebarView.addHandlerShowNavProjects();
-
-  ProjectView.addHandlerTasksAll(TaskController.ctrlShowAllTasks);
-  ProjectView.addHandlerTasksFinished(SidebarController.ctrlTasksFinished);
-  ProjectView.addHandlerTasksToday(SidebarController.ctrlTasksToday);
-  ProjectView.addHandlerTasksUpcoming(SidebarController.ctrlTasksUpcoming);
-  ProjectView.addHandlerHighPriority(SidebarController.ctrlHighPriority);
-  ProjectView.addHandlerMediumPriority(SidebarController.ctrlMediumPriority);
-  ProjectView.addHandlerLowPriority(SidebarController.ctrlLowPriority);
-
-  // Note initializer
-  NoteView.addHandlerShowNotes(NoteController.ctrlShowNotes);
-
-  // Modal initializers
-  AddTaskView.addHandlerAddTask(TaskController.ctrlAddTask);
-  AddTaskView.addHandlerListProjects(ProjectController.ctrlProjectsOnForm);
-  AddNoteView.addHandlerAddNote(NoteController.ctrlAddNote);
-  AddProjectView.addHandlerAddProject(ProjectController.ctrlAddProject);
+export const init = () => {
+  // Auth intializer
+  AuthController.initializeAuth();
 };
 
 init();
